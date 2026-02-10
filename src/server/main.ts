82572c694bd576fcd382daf30cd4ba582e2641b4
Lilyfc <coralcatcodes@gmail.com>
2026-02-10 13:20:17 -0500

    add Claude's message to message history

--- src/server/main.ts
+++ src/server/main.ts
@@ -33,16 +33,22 @@ let messageHistory: Message[] = []
 
 // // chat endpoint
 app.post("/chat", (req, res) => {
-  console.log('req body:',req.body)
+  // console.log('req body:',req.body)
   let userMessage = req.body.message
   const forMessage: Message = {role: 'user', content: userMessage}
   messageHistory.push(forMessage)
+  console.log('messageHistory 1:', messageHistory)
    async function main(){
       const message = await client.messages.create({
         max_tokens: 1024,
         messages: messageHistory,
         model: 'claude-haiku-4-5-20251001'
       })
+      // console.log('message:',message)
+      const claudeMessage: Message = {role: 'assistant', content: message.content[0].text}
+      console.log('claudeMessage:', claudeMessage)
+      messageHistory.push(claudeMessage)
+      console.log('messageHistory 2:', messageHistory)
       res.json(message)
     }
     main().catch(console.error)
