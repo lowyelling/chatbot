1edbab9fb22b812f69b17a66abbe65beaa829b60
Lilyfc <coralcatcodes@gmail.com>
2026-02-10 12:11:07 -0500

    update to Anthropic SDK

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,43 +1,66 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import dotenv from "dotenv";
+import Anthropic from "@anthropic-ai/sdk"
 dotenv.config()
 const PORT = 3000
 const app = express();
+const client = new Anthropic({
+    apiKey: process.env['ANTHROPIC_API_KEY']
+  }
+)
+
+// SDK
+app.get("/hello", (req, res) => {
+  async function main(){
+      const message = await client.messages.create({
+        max_tokens: 1024,
+        messages: [{role: 'user', content: 'Hi Claude! I am in NYC!'}],
+        model: 'claude-haiku-4-5-20251001'
+      })
+      res.json(message)
+    }
+    main().catch(console.error)
+  })
+
+
+// Node Fetch
+// app.get("/hello", (req, res) => {
+//   async function main(){
+//       const body = {
+//          "model": "claude-haiku-4-5-20251001",
+//           "max_tokens": 1000,
+//           "messages": [
+//               {
+//                 "role": "user", 
+//                 "content": `What is the capital of America? Tell me some facts`
+//               }
+//           ]
+//       }
+
+//    const response = await fetch('https://api.anthropic.com/v1/messages', 
+//     { method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'x-api-key': process.env.ANTHROPIC_API_KEY!,
+//         'anthropic-version': '2023-06-01'
+//         },
+//       body: JSON.stringify(body)
+//     })
+//     const data = await response.json()
+//     console.log('data:', data)
+//     res.json(data)
+//   }
+
+//   main().catch(console.error)
+// })
+
+// basic hello
 
 // app.get("/hello", (_, res) => {
 //   res.send("Hello Vite + React + TypeScript!");
 // });
 
-app.get("/hello", (req, res) => {
-  async function main(){
-      const body = {
-         "model": "claude-haiku-4-5-20251001",
-          "max_tokens": 1000,
-          "messages": [
-              {
-                "role": "user", 
-                "content": `What is the capital of America? Tell me some facts`
-              }
-          ]
-      }
-
-   const response = await fetch('https://api.anthropic.com/v1/messages', 
-    { method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': process.env.ANTHROPIC_API_KEY!,
-        'anthropic-version': '2023-06-01'
-        },
-      body: JSON.stringify(body)
-    })
-    const data = await response.json()
-    console.log('data:', data)
-    res.json(data)
-  }
-
-  main().catch(console.error)
-})
 
 ViteExpress.listen(app, PORT, () =>
   console.log(`Server is listening on port ${PORT}`),
