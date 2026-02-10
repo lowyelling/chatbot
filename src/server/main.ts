2999c88896fe2b6b75185a89fe2abbc8655180b5
Lilyfc <coralcatcodes@gmail.com>
2026-02-10 13:06:06 -0500

    user message works with curl

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,13 +2,58 @@ import express from "express";
 import ViteExpress from "vite-express";
 import dotenv from "dotenv";
 import Anthropic from "@anthropic-ai/sdk"
+
 dotenv.config()
+
 const PORT = 3000
 const app = express();
 const client = new Anthropic({
     apiKey: process.env['ANTHROPIC_API_KEY']
   }
 )
+
+app.use(express.json())
+
+export type Message = {
+  role: 'user' | 'assistant';
+  content: string
+}
+
+// declarations outside route to survive between requests
+let messageHistory: Message[] = []
+
+//  messages: [{role: 'user', content: 'Hi Claude! I am in NYC!'}]
+
+// chatendpoint toy
+// app.post("/chat", (req, res) => {
+//   console.log('req body:',req.body)
+//   res.json({'status': 'ok'})
+// })
+
+
+// // chat endpoint
+app.post("/chat", (req, res) => {
+  console.log('req body:',req.body)
+  let userMessage = req.body.message
+  const forMessage: Message = {role: 'user', content: userMessage}
+  messageHistory.push(forMessage)
+   async function main(){
+      const message = await client.messages.create({
+        max_tokens: 1024,
+        messages: messageHistory,
+        model: 'claude-haiku-4-5-20251001'
+      })
+      res.json(message)
+    }
+    main().catch(console.error)
+  })
+
+
+
+// // reset endpoint
+// app.get("/reset", (req, res) => {
+
+// })
 
 // SDK
 app.get("/hello", (req, res) => {
