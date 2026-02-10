a867752f493569595bd6193bf4cd32df6b8d51d4
Lilyfc <coralcatcodes@gmail.com>
2026-02-10 11:59:54 -0500

    update hello endpoint with fetch function to call Claude API

--- src/server/main.ts
+++ src/server/main.ts
@@ -5,9 +5,39 @@ dotenv.config()
 const PORT = 3000
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
+// app.get("/hello", (_, res) => {
+//   res.send("Hello Vite + React + TypeScript!");
+// });
+
+app.get("/hello", (req, res) => {
+  async function main(){
+      const body = {
+         "model": "claude-haiku-4-5-20251001",
+          "max_tokens": 1000,
+          "messages": [
+              {
+                "role": "user", 
+                "content": `What is the capital of America? Tell me some facts`
+              }
+          ]
+      }
+
+   const response = await fetch('https://api.anthropic.com/v1/messages', 
+    { method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.ANTHROPIC_API_KEY!,
+        'anthropic-version': '2023-06-01'
+        },
+      body: JSON.stringify(body)
+    })
+    const data = await response.json()
+    console.log('data:', data)
+    res.json(data)
+  }
+
+  main().catch(console.error)
+})
 
 ViteExpress.listen(app, PORT, () =>
   console.log(`Server is listening on port ${PORT}`),
