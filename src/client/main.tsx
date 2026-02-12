a6254407e4a4f0a5c305c44a66059277deb2f739
Lilyfc <coralcatcodes@gmail.com>
2026-02-12 09:41:20 -0500

    install react-router

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,9 +2,12 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
