9a04c0b6868c1712cdb7cc8c70ece94bd972df6c
Lilyfc <coralcatcodes@gmail.com>
2026-02-12 09:51:51 -0500

    configure main.tsx route - verify render is the same

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,12 +2,14 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element = { <App />} />
+        </Routes>
     </BrowserRouter>
 );
