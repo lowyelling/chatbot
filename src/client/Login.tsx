287a3596294024cf397b39c49cfa1996c00cf03d
Lilyfc <coralcatcodes@gmail.com>
2026-02-13 12:14:46 -0500

    setup basic Login page wiring

--- src/client/Login.tsx
+++ src/client/Login.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { authClient } from "../lib/auth-client"
 
-import { Input } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -16,19 +16,73 @@ import {
 } from "@/components/ui/card"
 
 function Login(){
-
-    // state for email, password, whether user is signing up or signing in (boolean like isSignUp)
+    const navigate = useNavigate()
+    // state for email, password, and whether user is signing up or signing in (boolean like isSignUp)
     // use useNavigate for redirecting after success
 
+    const [email, setEmail] = useState("") // to wire up Input as controlled input
+    const [password, setPassword] = useState("") // to wire up Input as controlled input
+    const [isSignUp, setIsSignUp] = useState(true)
+    const [error, setError] = useState<string | null>(null)
 
+    async function signIn(){
+        await authClient.signIn.email({
+            email, // user email address
+            password // user password -> min 8 characters as default
+        }, {
+            onSuccess: () => navigate("/new"),
+            onError: (ctx) => setError(ctx.error.message) //ctx = context, an Object passed to callback with ctx.error.message or .status etc
+        })
+    }
+
+    async function signUp(){
+        await authClient.signUp.email({
+            email, // user email address
+            password // user password -> min 8 characters as default
+            // name
+        }, {
+            onSuccess: () => navigate("/new"),
+            onError: (ctx)=>setError(ctx.error.message)
+        }         
+        )
+    }
+
+    async function signWithGitHub(){
+        await authClient.signIn.social({ provider: "github", callbackURL: "/new"},{
+            onSuccess: () => navigate("/new"),
+            onError: (ctx) => setError(ctx.error.message)
+        })
+    }
 
 
     return (
-        <div>Hi</div>
+        <>
+        {isSignUp ? (
+        <div>
+            <h1 className="text-2xl font-bold text-center mb-4">Login</h1>
+            <Input type="email" placeholder="Email" value={email} onChange={(event)=>setEmail(event.target.value)}/>
+            <Input type="password" placeholder="Password" value={password} onChange={(event)=>setPassword(event.target.value)}/>
+            <Button onClick={()=>signIn()}>Login</Button>
+            <Button onClick={()=>signWithGitHub()}>Sign-in with Github</Button>
+        </div>)
+        :
 
-    // form that calls two functions - one for signin and one for signup
+        (
+        <div>
+            <h1 className="text-2xl font-bold text-center mb-4">Sign Up</h1>
+            <Input type="email" placeholder="Email" value={email} onChange={(event)=>setEmail(event.target.value)}/>
+            <Input type="password" placeholder="Password" value={password} onChange={(event)=>setPassword(event.target.value)}/>
+            <Button onClick={()=>signUp()}>Sign Up</Button>
+            <Button onClick={()=>signWithGitHub()}>Sign up with Github</Button>
+        </div>)
+        }
 
-    // Github button that calls the signin 
+        <button onClick={()=> setIsSignUp(prev=>!prev)}>
+            {isSignUp ? "Don't have an account? Sign up" : "Already have an account? Sign in"}
+        </button>
+        </>
 
     )
 }
+
+export default Login
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router";
 
 import App from "./App";
+import Login from "./Login";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <BrowserRouter>
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Link to="/chat/:conversationId">chats</Link>
         <Link to="/new">new</Link> */}
         <Routes>
-            <Route path="/" element = { <App />} />
+            <Route path="/" element = { <Login />} />
             <Route path="/chat/:chatId" element = { <App />} />
             <Route path="/new" element = { <App />} />
         </Routes>
