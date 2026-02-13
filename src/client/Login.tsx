b25ce0433df87be066739abb4e1395056c9d0cbb
Lilyfc <coralcatcodes@gmail.com>
2026-02-13 11:15:57 -0500

    setup Login component with skeleton

---
+++ src/client/Login.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { authClient } from "../lib/auth-client"
+
+import { Input } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+function Login(){
+
+    // state for email, password, whether user is signing up or signing in (boolean like isSignUp)
+    // use useNavigate for redirecting after success
+
+
+
+
+    return (
+        <div>Hi</div>
+
+    // form that calls two functions - one for signin and one for signup
+
+    // Github button that calls the signin 
+
+    )
+}
